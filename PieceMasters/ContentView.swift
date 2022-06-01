0f09ef07bab91f1429a1e7b3342586f170ab2251
shahadq <shahad.mq20@gmail.com>
2022-06-01 09:56:49 +0300

    Initial Commit

---
+++ PieceMasters/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PieceMasters
+//
+//  Created by Shahad on 02/11/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PieceMasters/PieceMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieceMastersApp.swift
+//  PieceMasters
+//
+//  Created by Shahad on 02/11/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct PieceMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
