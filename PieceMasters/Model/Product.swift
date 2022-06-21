368b9c2b68c85a3ddba95bd83077494e3c3f6a94
shahadq <shahad.mq20@gmail.com>
2022-06-21 03:13:33 +0300

    ....

---
+++ PieceMasters/Model/Product.swift
@@ -0,0 +1,21 @@
+//
+//  Product.swift
+//  PieceMasters
+//
+//  Created by Shahad on 22/11/1443 AH.
+//
+
+import Foundation
+
+struct product : Identifiable{
+    
+    var id : String
+    var name: String
+    var Description: String
+    var size: String
+    var Cardboard: String
+    var image: String
+    var Category: String
+    var price: Double
+    var is_Customizable: String
+}
