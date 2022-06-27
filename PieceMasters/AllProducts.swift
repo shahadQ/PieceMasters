f4cf119a611beff7e050de1ce936de82c666f0c7
Eman Mohammed <40224378+Emanmo7med@users.noreply.github.com>
2022-06-27 03:17:01 +0300

    .

--- PieceMasters/AllProducts.swift
+++ PieceMasters/AllProducts.swift
@@ -43,7 +43,7 @@ struct ProductView: View {
     @State var categoryIndexzx = 0
     @State var text = ""
     @State var filterData : [product] = []
-    
+    @State private var searchQuery = ""
     var body: some View {
       
         ZStack {
@@ -55,14 +55,16 @@ struct ProductView: View {
                 ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
                     
                     Text("Packaging and boxes")
-                    
+                        .padding(.top, -50)
                         .font(.title2)
                         .foregroundColor(Color(red: 0.22, green: 0.26, blue: 0.478))
-                    SearchBar(text: $text)
-                        .padding(.top, 30)
+                    SearchBar(text: $searchQuery)
+                        .padding(.top, 10)
+
                     
                 }
                 Text("Browse by category")
+                    .padding(.top, 40)
                     .foregroundColor(Color(red: 0.42, green: 0.416, blue: 0.42))
                 
                 
@@ -82,7 +84,8 @@ struct ProductView: View {
                         
                 }
                 .foregroundColor(Color(red: 0.22, green: 0.26, blue: 0.478))
-                
+                // case1
+                if searchQuery == ""{
                 ScrollView(.vertical, showsIndicators: false){
                     LazyVGrid(columns: columns, spacing:40){
                         ForEach(categoryIndexzx == 0 ? Productviewmodel.list : Productviewmodel.list.filter({ p in
@@ -124,10 +127,8 @@ struct ProductView: View {
                                                 
                                                 VStack (alignment: .leading){
                                                     Text(product.title)
-                                                        .font(.title3)
-                                                        .fontWeight(.bold)
+                                                        .font(.system(size: 16, weight: .bold, design: .default))
                                                         .foregroundColor(Color("mainfont"))
-                                                       
                                                     Text(product.amount)
                                                         .font(.caption)
                                                         .foregroundColor(Color("subfont"))
@@ -143,7 +144,8 @@ struct ProductView: View {
                                                         
                                                         Spacer()
                                                         Text("\(product.price) RS")
-                                                            .font(.caption)
+                                                            .font(.system(size: 13, weight: .bold, design: .default))
+
                                                             .foregroundColor(Color("mainfont"))
                                                             .fontWeight(.semibold)
                                                     }
@@ -189,11 +191,117 @@ struct ProductView: View {
                     }
                 }
                 .padding(.top, -2.0)
+                }
+
+                
+                 else if  searchQuery != ""{
+                ScrollView(.vertical, showsIndicators: false){
+                    LazyVGrid(columns: columns, spacing:40){
+                        ForEach( searchQuery == "" ? Productviewmodel.list:
+                                        Productviewmodel.list.filter{ $0.title.lowercased().contains(searchQuery.lowercased())})
+                        
+                        { product in
+                            NavigationLink(destination: ProductDetails(Product: product)){
+                            ZStack{
+                                
+                                
+                                Button(action: {
+
+                                }){
+                                    HStack{
+                                        Image(systemName: product.isSelected ? "bookmark.fill": "bookmark")
+                                        
+                                            .foregroundColor(Color(red: 0.5, green: 0.8, blue: 0.736))
+                                    }
+                                }
+                                .position(x: 130, y: 25)
+                                
+                                ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)){
+                                    VStack {
+                                        //
+                                        Image("\(product.image)")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(height: 150)
+                                            .padding()
+                                        
+                                        
+                                        
+                                        VStack{
+                                            HStack {
+                                                
+                                                VStack (alignment: .leading){
+                                                    Text(product.title)
+                                                        .font(.system(size: 16, weight: .bold, design: .default))
+                                                        .foregroundColor(Color("mainfont"))
+                                                    Text(product.amount)
+                                                        .font(.caption)
+                                                        .foregroundColor(Color("subfont"))
+                                                        .minimumScaleFactor(0.9)
+                                                        .lineLimit(2)
+                                                        
+                                                    
+                                                    HStack{
+                                                        Text("Start from")
+                                                            .font(.subheadline)
+                                                            .foregroundColor(Color(red: 0.5, green: 0.8, blue: 0.736))
+                                                            .fontWeight(.light)
+                                                        
+                                                        Spacer()
+                                                        Text("\(product.price) RS")
+                                                            .font(.system(size: 13, weight: .bold, design: .default))
+                                                            .foregroundColor(Color("mainfont"))
+                                                            .fontWeight(.semibold)
+                                                    }
+                                                    
+                                                }
+                                                
+                                                Spacer()
+                                            }
+                                        }
+                                        .padding(.horizontal, 10)
+                                        .padding(.vertical,20)
+                                        
+                                        .background(.white)
+                                        
+                                        
+                                    }
+                                    
+                                    
+                                    
+                                }
+                                
+                                
+                                ZStack{
+                                    
+                                    
+                                    Image(product.offer != nil ? "offer" : "")
+                                        .padding()
+                                        .position(x: 30, y: 30)
+                                }
+                                }
+                                }
+                              
+                            
+                            
+                            .background(Color("iColor1"))
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                            .shadow(color: Color("iColor1").opacity(1.5), radius: 5, x:0, y: 5)
+
+                            .frame(width: 150, height: 290)
+
+                            
+                        }
+                    }
+                }
+                .padding(.top, -2.0)
+                }
+                
                 
                 Spacer()
             }.padding(.top, 20)
-        }
-        .padding()
+        }.padding()
+        .padding(.top ,-10)
         
         
         
@@ -244,22 +352,49 @@ struct Categories: View {
             default:
                 isfilterArray = false
             }
-        }, label: { VStack(spacing: 8 ){
+        }, label: { VStack(spacing:0){
             if (isfilterArray == false){
-                
-                
+//
+//
+//                Text(categories[data])
+//                    .font(.system(size: 22))
+//                    .fontWeight(index == data ? .bold : .none)
+//                    .foregroundColor(Color(index == data ? "mainfont" : "subfont"))
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(index == data ? "mainfont" : "BACGR1"))
+                        .frame(width: 110, height: 36)
                 Text(categories[data])
-                    .font(.system(size: 22))
+                    
+                    .font(.system(size: 18))
                     .fontWeight(index == data ? .bold : .none)
-                    .foregroundColor(Color(index == data ? "mainfont" : "subfont"))
+                    .foregroundColor(Color(index == data ? .white : .gray))
+                    
+
+
+                }
             }
             else
             {
                 
+//                Text(categories[data])
+//                    .font(.system(size: 22))
+//                    .fontWeight(index == data ? .bold : .none)
+//                    .foregroundColor(Color(index == data ? "mainfont" : "subfont"))
+                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(index == data ? "mainfont" : "BACGR1"))
+                        .frame(width: 110, height: 36)
                 Text(categories[data])
-                    .font(.system(size: 22))
+                    
+                    .font(.system(size: 18))
                     .fontWeight(index == data ? .bold : .none)
-                    .foregroundColor(Color(index == data ? "mainfont" : "subfont"))
+                    .foregroundColor(Color(index == data ? .white : .gray))
+                    
+
+
+                }
                 
             }
             
@@ -267,11 +402,11 @@ struct Categories: View {
             
             
             
-            Capsule()
-                .fill(Color("mainfont"))
-                .frame(width: 30, height: 4)
-                
-                .opacity(index == data ? 1 : 0)
+//            Capsule()
+//                .fill(Color(.white))
+//                .frame(width: 30, height: 4)
+//
+//                .opacity(index == data ? 1 : 0)
         }.onTapGesture {
            
             index = data
@@ -280,3 +415,4 @@ struct Categories: View {
         
     }
 }
+
